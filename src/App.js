835b81529afdcb669463144121a198a69a8ed029
shubham chand <thakurchand98@gmail.com>
2023-03-16 17:07:21 +0530

    nav bar
    Signed-off-by: shubham chand <thakurchand98@gmail.com>

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ function App() {
       <nav className="navbar navbar-expand-lg bg-body-tertiary">
         <div className="container-fluid">
           <a className="navbar-brand" href="/">
-            Navbar
+            Demo
           </a>
           <button
             className="navbar-toggler"
